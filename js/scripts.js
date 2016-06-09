1b1f875bc908278854efc7c0d1b8d0d18d692506
Maarja Laineste and Sami Al-Jamal <sami@>
2016-06-09 13:12:39 -0700

    return the value of number in romanNumeral except for double digit numbers with 4 or 9.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
 var romanNumberal= function(number){
-  var result;
+  var result= '';
+  var i;
   debugger;
 
-  var romanDict= {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000};
-  Object.keys(romanDict).forEach(function(key){
-    if(number === romanDict[key]){
-     result= key;
+  var romanDict= { M: 1000, D: 500,C: 100,L: 50, X: 10, IX: 9, V: 5, IV:4, I: 1 };
+
+  for (i in romanDict) {
+    while(number >= romanDict[i]) {
+      result += i;
+      number -= romanDict[i];
     }
+  }
 
-  });
-  return result;
-};
+  // var str = new Array();
+  // var str = number.toString();
+  //
+  // str.forEach(function(num) {
+  //   if ()
+  // })
 
+    return result;
 
-
-
-
-
-
+ };
 
 $(document).ready(function() {
   $("form#roman").submit(function(event) {
